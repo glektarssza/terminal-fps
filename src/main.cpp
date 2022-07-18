fdd2ec6ddd0e641b670353f47de53258890a5b44
G'lek Tarssza <glek@glektarssza.com>
2022-07-18 06:56:57 -0600

    Added a simple main.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file src/main.cpp
+ * @author G'lek Tarssza
+ * @copyright Copyright (c) 2022 G'lek Tarssza
+ * @copyright Licensed under the MIT License.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * The program entry point.
+ *
+ * @param argc The number of elements in the `argv` array.
+ * @param argv The commend line arguments as an array of null-terminated
+ * strings.
+ */
+int main(int argc, char** argv) {
+    std::cout << "Hello world!" << std::endl;
+    return EXIT_SUCCESS;
+}
