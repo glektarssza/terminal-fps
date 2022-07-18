2e40506c2452d5c38a43a1af242f092646b1b3d4
G'lek Tarssza <glek@glektarssza.com>
2022-07-18 08:57:48 -0600

    Added some texture methods.

--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -1,3 +1,31 @@
 #include "graphics/texture.hpp"
 
-namespace terminal_fps::graphics {} // namespace terminal_fps::graphics
+#include <stdexcept>
+
+namespace terminal_fps::graphics {
+    uint16_t Texture::GetWidth() const {
+        return m_width;
+    }
+
+    uint16_t Texture::GetHeight() const {
+        return m_height;
+    }
+
+    uint32_t Texture::GetPixelCount() const {
+        return m_width * m_height;
+    }
+
+    Color* Texture::GetPixel(uint16_t x, uint16_t y) const {
+        if (x >= m_width || y >= m_height) {
+            throw std::out_of_range("Coordinates out of range");
+        }
+        return &m_pixels[y * m_width + x];
+    }
+
+    void Texture::SetPixel(uint16_t x, uint16_t y, const Color& value) {
+        if (x >= m_width || y >= m_height) {
+            throw std::out_of_range("Coordinates out of range");
+        }
+        m_pixels[y * m_width + x] = value;
+    }
+} // namespace terminal_fps::graphics
