4902b5bd083b73b830c850d5dcfcdec769bcafe2
G'lek Tarssza <glek@glektarssza.com>
2024-01-10 12:34:31 -0700

    Simple main.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * The program entry point.
+ *
+ * @param argc The number of elements in the `argv` array.
+ * @param argv An array of C-style strings representing the command-line
+ * arguments passed to the program.
+ *
+ * @returns A status code for the operating system.
+ */
+int main(int argc, char** argv) {
+    std::cout << "Hello world!" << std::endl;
+    return EXIT_SUCCESS;
+}
