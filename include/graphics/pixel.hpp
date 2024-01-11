9b75f145f2c4d2b2e640cd90478a64766659ecae
G'lek Tarssza <glek@glektarssza.com>
2024-01-11 07:58:40 -0700

    Added a pixel structure.

---
+++ include/graphics/pixel.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file include/graphics/pixel.hpp
+ * @author G'lek Tarssza
+ * @copyright Copyright (c) 2024 G'lek Tarssza
+ * @copyright All rights reserved.
+ */
+
+#pragma once
+
+//-- Project Code
+#include "graphics/color.hpp"
+
+namespace terminal_fps::graphics {
+    /**
+     * A simple representation of a pixel in an output device.
+     */
+    struct Pixel {
+        /**
+         * Get the default value.
+         *
+         * @returns The default value.
+         */
+        static Pixel getDefault();
+
+        /**
+         * The color of the pixel.
+         */
+        Color color;
+
+        /**
+         * The symbol that represents the pixel.
+         */
+        char16_t symbol;
+    };
+} // namespace terminal_fps::graphics
---
+++ src/graphics/pixel.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file src/graphics/pixel.cpp
+ * @author G'lek Tarssza
+ * @copyright Copyright (c) 2024 G'lek Tarssza
+ * @copyright All rights reserved.
+ */
+
+//-- Project Code
+#include "graphics/pixel.hpp"
+
+namespace terminal_fps::graphics {
+    Pixel Pixel::getDefault() {
+        return {Color::getDefault(), u'\u2588'};
+    }
+} // namespace terminal_fps::graphics
